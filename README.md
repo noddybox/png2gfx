af47c4ea1f517026172b105c6eb200373c15ba89
Ian C <ianc@noddybox.co.uk>
2026-09-15 23:05:18 +0100

    Initial working version

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # png2gfx
-A converter for PNG to the simple bitmap format used by espec and ezx81.
+A converter for PNG to the simple bitmap format used by espec and ezx81.  The
+format is best explained with this comment.
+
+```
+/* Draws a simply compressed bitmap.  The data is in the form (where b is a
+   byte from the stream):
+
+    	b < 0x80	Colour (0 Black, 1 Blue, 2 Red, 3 Magenta, 4 Green,
+				5 Cyan, 6 Yellow, 7 White, 8 Grey)
+
+	b >= 0x80	Repeat the last colour b-0x80 times.
+*/
+```
 
 Usage: `png2gfx input.png outfile`
--- png2gfx.c
+++ png2gfx.c
@@ -18,8 +18,19 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+#include <limits.h>
 
 #include <png.h>
+
+#define BLACK	0
+#define BLUE	1
+#define RED	2
+#define MAGENTA	3
+#define GREEN	4
+#define CYAN	5
+#define YELLOW	6
+#define WHITE	7
+#define GREY	8
 
 #define HEADER_SIZE (8)
 
@@ -85,7 +96,100 @@ int main(int argc, char *argv[])
     png_init_io(png, in);
     png_set_sig_bytes(png, HEADER_SIZE);
 
-    png_read_png(png, info, PNG_TRANSFORM_EXPAND, NULL);
+    png_read_png(png, info,
+    		 PNG_TRANSFORM_EXPAND|PNG_TRANSFORM_STRIP_16|
+		 PNG_TRANSFORM_STRIP_ALPHA|PNG_TRANSFORM_PACKING,
+		 NULL);
+
+    fclose(in);
+
+    int width;
+    int height;
+    int depth;
+    int colour_type;
+
+    png_get_IHDR(png, info, &width, &height, &depth, &colour_type,
+    		 NULL, NULL, NULL);
+
+    switch (colour_type)
+    {
+    	case PNG_COLOR_TYPE_GRAY:
+	case PNG_COLOR_TYPE_GRAY_ALPHA:
+	    fprintf(stderr, "%s: grey scale image not supported\n", base);
+	    fclose(in);
+	    return EXIT_FAILURE;
+
+	default:
+	    break;
+    }
+
+    png_bytepp rows = png_get_rows(png, info);
+
+    unsigned char *dest = malloc(width * height);
+
+    if (!dest)
+    {
+    	fprintf(stderr, "%s: failed to allocate buffer for destination image\n",
+				base);
+    }
+
+    unsigned char *writer = dest;
+
+    for(int y = 0; y < height; y++)
+    {
+	for(int x = 0; x < width; x++)
+	{
+	    int r,g,b;
+
+	    r = rows[y][x * 3 + 0];
+	    g = rows[y][x * 3 + 1];
+	    b = rows[y][x * 3 + 2];
+
+	    unsigned char bmp_pix = 0;
+
+	    if (r == 0 && g == 0 && b > 0)
+	    {
+	    	bmp_pix = BLUE;
+	    }
+	    else if (r > 0 && g == 0 && b == 0)
+	    {
+	    	bmp_pix = RED;
+	    }
+	    else if (r > 0 && g == 0 && b > 0)
+	    {
+	    	bmp_pix = MAGENTA;
+	    }
+	    else if (r == 0 && g > 0 && b == 0)
+	    {
+	    	bmp_pix = GREEN;
+	    }
+	    else if (r == 0 && g > 0 && b > 0)
+	    {
+	    	bmp_pix = CYAN;
+	    }
+	    else if (r > 0 && g > 0 && b == 0)
+	    {
+	    	bmp_pix = YELLOW;
+	    }
+	    else if (r == g && g == b)
+	    {
+		if (r < 0x20)
+		{
+		    bmp_pix = BLACK;
+		}
+		else if (r > 0xa0)
+		{
+		    bmp_pix = WHITE;
+		}
+		else
+		{
+		    bmp_pix = GREY;
+		}
+	    }
+
+	    *writer++ = bmp_pix;
+	}
+    }
 
     FILE *out;
 
@@ -94,6 +198,49 @@ int main(int argc, char *argv[])
     	perror(argv[2]);
 	return EXIT_FAILURE;
     }
+
+    unsigned char *reader = dest;
+    int len = width * height;
+    int count = 0;
+    unsigned char last = UCHAR_MAX;
+
+    while(len--)
+    {
+    	if (last == *reader)
+	{
+	    if (count < 0x7f)
+	    {
+	    	count++;
+	    }
+	    else
+	    {
+	    	fputc(0x80 + count, out);
+	    	fputc(last, out);
+		count = 0;
+	    }
+	}
+	else
+	{
+	    if (count > 0)
+	    {
+	    	fputc(0x80 + count, out);
+		count = 0;
+	    }
+
+	    fputc(*reader, out);
+	}
+
+	last = *reader++;
+    }
+
+    if (count > 0)
+    {
+	fputc(0x80 + count, out);
+    }
+
+    fclose(out);
+
+    free(dest);
 
     return EXIT_SUCCESS;
 }
